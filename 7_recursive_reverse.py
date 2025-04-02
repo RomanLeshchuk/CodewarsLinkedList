a2ae0d8d9bafdddb2fa273ef6c7a38fedd7d3c13
RomanLeshchuk <romanleshchuk2000@gmail.com>
2025-04-02 15:58:39 +0300

    Seventh problem, first try

---
+++ 7_recursive_reverse.py
@@ -0,0 +1,21 @@
+class Node(object):
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+def reverse(head):
+    if head.next is None:
+        return head
+
+    pre_last_node = None
+    last_node = head
+    while last_node.next is not None:
+        pre_last_node = last_node
+        last_node = last_node.next
+
+    if pre_last_node is not None:
+        pre_last_node.next = None
+
+    last_node.next = reverse(head)
+
+    return last_node
