81a4c65d285822ff61aa340603cd55d18ee0d3bf
RomanLeshchuk <romanleshchuk2000@gmail.com>
2025-04-02 16:42:19 +0300

    Tenth problem, second try, fixed throw error logic and also changed recursive call argument, passed

--- 10_alternating_split.py
+++ 10_alternating_split.py
@@ -9,15 +9,22 @@ class Context(object):
         self.second = second
 
 def alternating_split(head):
+    def recursion_split(head):
+        if head is None or head.next is None:
+            res = Context(head, None)
+            if head.next is not None:
+                res.second = head.next
+                head.next = None
+            return res
+
+        curr_res = Context(head, head.next)
+        next_res = recursion_split(head.next.next)
+        curr_res.first.next = next_res.first
+        curr_res.second.next = next_res.second
+
+        return curr_res
+
     if head is None or head.next is None:
-        res = Context(head, head.next)
-        if res.first.next is not None:
-            res.first.next = None
-        return res
+        raise RuntimeError("Start list should have at least two elements")
 
-    curr_res = Context(head, head.next)
-    next_res = alternating_split(head)
-    curr_res.first.next = next_res.first
-    curr_res.second.next = next_res.second
-
-    return curr_res
+    return recursion_split(head)
