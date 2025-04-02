82a157976eacf624d822590cc02ae92df50f636e
RomanLeshchuk <romanleshchuk2000@gmail.com>
2025-04-02 16:17:56 +0300

    Ninth problem, first try, passed

---
+++ 9_swap_pairs.py
@@ -0,0 +1,17 @@
+# from preloaded import Node
+
+class Node:
+    def __init__(self, next=None):
+        self.next = next
+
+def swap_pairs(head):
+    if head is None or head.next is None:
+        return head
+
+    new_head = head.next
+    head.next = new_head.next
+    new_head.next = head
+
+    head.next = swap_pairs(head.next)
+
+    return new_head
